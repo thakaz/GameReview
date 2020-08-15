8cc26005d8aefa4be89dc650e835a682087f784e
thakaz <kusuriuri12121@yahoo.co.jp>
2020-08-15 15:15:16 +0900

    とりあえず画像

--- GameReview/Controllers/GamesController.cs
+++ GameReview/Controllers/GamesController.cs
@@ -7,16 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GameReview.Data;
 using GameReview.Models;
+using GameReview.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace GameReview.Controllers
 {
     public class GamesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public GamesController(ApplicationDbContext context)
+        public GamesController(ApplicationDbContext context,IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostingEnvironment = hostEnvironment;
         }
 
         // GET: Games
@@ -54,15 +60,52 @@ namespace GameReview.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Title,Developer,Publisher,ReleaseDate,ImagePath")] Game game)
+        public async Task<IActionResult> Create(GameReviewVM gameReviewVM)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(game);
+                _context.Add(gameReviewVM.Game);
+                _context.Add(gameReviewVM.ReviewerGameReview);
+
+
+                string result = "";
+                IFormFile postedFile = gameReviewVM.ImageFile;
+                if (postedFile != null && postedFile.Length > 0)
+                {
+                    // アップロードされたファイル名を取得。ブラウザが IE 
+                    // の場合 postedFile.FileName はクライアント側でのフ
+                    // ルパスになることがあるので Path.GetFileName を使う
+                    string filename =
+                                  Path.GetFileName(postedFile.FileName);
+
+                    // アプリケーションルートの物理パスを取得。Core では
+                    // Server.MapPath は使えないので以下のようにする
+                    string contentRootPath =
+                                    _hostingEnvironment.ContentRootPath;
+                    string filePath = contentRootPath + "\\" +
+                                      "UploadedFiles\\" + filename;
+
+                    using (var stream =
+                                new FileStream(filePath, FileMode.Create))
+                    {
+                        await postedFile.CopyToAsync(stream);
+                    }
+
+                    result = filename + " (" + postedFile.ContentType +
+                             ") - " + postedFile.Length +
+                             " bytes アップロード完了";
+                }
+                else
+                {
+                    result = "ファイルアップロードに失敗しました";
+                }
+
+
+
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(game);
+            return View(gameReviewVM);
         }
 
         // GET: Games/Edit/5
---
+++ GameReview/Models/ViewModel/GameReviewVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+namespace GameReview.Models.ViewModel
+{
+    public class GameReviewVM
+    {
+        public Game Game { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public ReviewerGameReview ReviewerGameReview { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+
+
+    }
+}
